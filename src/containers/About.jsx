59712c3be6aeaa08de925602bd3e16bc545b5878
Aman Ibrahim <amanmibra@gmail.com>
2020-04-19 08:08:40 -0400

    Update flag

--- src/containers/About.jsx
+++ src/containers/About.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {
   Container,
   Divider,
+  Flag,
   Icon,
   Item,
   Segment,
@@ -27,7 +28,7 @@ class About extends Component {
                     <Item.Image rounded src={aman} size="medium"/>
 
                     <Item.Content verticalAlign="middle">
-                      <Item.Header>Aman Ibrahim 🇪🇷</Item.Header>
+                      <Item.Header>Aman Ibrahim <Flag name='er'/></Item.Header>
                       <Item.Meta>Machine Learning Platform @ <b>Cruise</b></Item.Meta>
                       <Divider />
                       <Item.Description>
