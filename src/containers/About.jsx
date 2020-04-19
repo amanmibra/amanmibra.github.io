9423217286510df928e341640acb3ba5956acb66
Aman Ibrahim <amanmibra@gmail.com>
2020-04-19 06:17:18 -0400

    Added transition animation to about page

--- src/containers/About.jsx
+++ src/containers/About.jsx
@@ -5,6 +5,7 @@ import {
   Icon,
   Item,
   Segment,
+  Transition,
 } from 'semantic-ui-react';
 
 import NavBar from './../components/navbar';
@@ -19,41 +20,43 @@ class About extends Component {
         <NavBar />
         <div className="page-body">
           <Container>
-            <Segment>
-              <Item.Group divided>
-                <Item>
-                  <Item.Image rounded src={aman} size="medium"/>
+            <Transition transitionOnMount duration={2000}>
+              <Segment>
+                <Item.Group divided>
+                  <Item>
+                    <Item.Image rounded src={aman} size="medium"/>
 
-                  <Item.Content verticalAlign="middle">
-                    <Item.Header>Aman Ibrahim 🇪🇷</Item.Header>
-                    <Item.Meta>Machine Learning Platform @ <b>Cruise</b></Item.Meta>
-                    <Divider />
-                    <Item.Description>
-                      <div>
-                        <p>
-                          I grew up in Raleigh, NC, and graduated from Duke on May 2020 with a Bachelor of Science in Computer Science.
-                        </p>
-                        <p>
-                          I have great experince in Full Stack Web Development as I have worked in the front-end, back-end, and DevOps on multiple web applications. I have worked on projects on all stages from early planning to testing to launch and production.
-                        </p>
-                        <p>
-                          My current interest is in the sphere of Artifical Intelligence, in particular to Machine Learning and Natural Language Processing. I am looking towards gaining more experince in this field through industry and graduate school.
-                        </p>
-                      </div>
-                    </Item.Description>
-                  </Item.Content>
-                </Item>
+                    <Item.Content verticalAlign="middle">
+                      <Item.Header>Aman Ibrahim 🇪🇷</Item.Header>
+                      <Item.Meta>Machine Learning Platform @ <b>Cruise</b></Item.Meta>
+                      <Divider />
+                      <Item.Description>
+                        <div>
+                          <p>
+                            I grew up in Raleigh, NC, and graduated from Duke on May 2020 with a Bachelor of Science in Computer Science.
+                          </p>
+                          <p>
+                            I have great experince in Full Stack Web Development as I have worked in the front-end, back-end, and DevOps on multiple web applications. I have worked on projects on all stages from early planning to testing to launch and production.
+                          </p>
+                          <p>
+                            My current interest is in the sphere of Artifical Intelligence, in particular to Machine Learning and Natural Language Processing. I am looking towards gaining more experince in this field through industry and graduate school.
+                          </p>
+                        </div>
+                      </Item.Description>
+                    </Item.Content>
+                  </Item>
 
-                <Item>
-                  <Item.Content>
-                    <a href="mailto:amanmibra@gmail.com">
-                      <Icon name="mail" />
-                      amanmibra@gmail.com
-                    </a>
-                  </Item.Content>
-                </Item>
-              </Item.Group>
-            </Segment>
+                  <Item>
+                    <Item.Content>
+                      <a href="mailto:amanmibra@gmail.com">
+                        <Icon name="mail" />
+                        amanmibra@gmail.com
+                      </a>
+                    </Item.Content>
+                  </Item>
+                </Item.Group>
+              </Segment>
+            </Transition>
           </Container>
         </div>
       </div>
